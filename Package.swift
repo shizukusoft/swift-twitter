98f7610aae34c6c8090f24ee1277dae92af9a318
Jaehong Kang <sinoru@me.com>
2021-02-23 20:17:17 +0900

    Define as dynamic framework

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "TwitterKit",
+            type: .dynamic,
             targets: ["TwitterKit"]),
     ],
     dependencies: [
