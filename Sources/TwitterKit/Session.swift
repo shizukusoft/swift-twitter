1b04a5beabca24d4510ba65862581d02fa988e7c
Jaehong Kang <sinoru@me.com>
2021-02-27 15:58:40 +0900

    Minor code refactoring on Session

--- Sources/TwitterKit/Session.swift
+++ Sources/TwitterKit/Session.swift
@@ -9,15 +9,11 @@ import Foundation
 import Alamofire
 
 public class Session {
-    public private(set) lazy var globalQueue = DispatchQueue(label: "\(String(reflecting: Session.self))", qos: .default, attributes: .concurrent)
-    public private(set) lazy var mainQueue = DispatchQueue(label: "\(String(reflecting: Session.self)).main", qos: .default, target: globalQueue)
+    public let consumerKey: String
+    let consumerSecret: String
 
-    public private(set) lazy var oauth1Authenticator = OAuth1Authenticator(session: self)
-    public var oauth1Credential: OAuth1Credential? {
-        get { oauth1AuthenticationInterceptor.credential }
-        set { oauth1AuthenticationInterceptor.credential = newValue }
-    }
-    private(set) lazy var oauth1AuthenticationInterceptor = AuthenticationInterceptor(authenticator: oauth1Authenticator)
+    public let globalQueue: DispatchQueue
+    public let mainQueue: DispatchQueue
 
     public private(set) lazy var alamofireSession = Alamofire.Session(
         configuration: URLSessionConfiguration.twtk_default,
@@ -26,11 +22,21 @@ public class Session {
         serializationQueue: globalQueue
     )
 
-    public let consumerKey: String
-    let consumerSecret: String
+    public private(set) lazy var oauth1Authenticator = OAuth1Authenticator(session: self)
+    public var oauth1Credential: OAuth1Credential? {
+        get { oauth1AuthenticationInterceptor.credential }
+        set { oauth1AuthenticationInterceptor.credential = newValue }
+    }
+    private(set) lazy var oauth1AuthenticationInterceptor = AuthenticationInterceptor(authenticator: oauth1Authenticator)
 
     public init(consumerKey: String, consumerSecret: String) {
         self.consumerKey = consumerKey
         self.consumerSecret = consumerSecret
+
+        let globalQueue = DispatchQueue(label: "\(String(reflecting: Session.self))", qos: .default, attributes: .concurrent)
+        let mainQueue = DispatchQueue(label: "\(String(reflecting: Session.self)).main", qos: .default, target: globalQueue)
+
+        self.globalQueue = globalQueue
+        self.mainQueue = mainQueue
     }
 }
