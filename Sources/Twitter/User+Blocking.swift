00f6522da435fd19f7ea5f1947f898d94ec10261
Jaehong Kang <sinoru@me.com>
2021-08-08 23:35:41 +0900

    Group as same extension

--- Sources/Twitter/User+Blocking.swift
+++ Sources/Twitter/User+Blocking.swift
@@ -26,9 +26,7 @@ extension User {
     public func blockingUsers(pageCount: Int16? = nil, paginationToken: String? = nil, session: Session) async throws -> Pagination<Result<User, TwitterServerError>> {
         try await Self.blockingUsers(forUserID: id, pageCount: pageCount, paginationToken: paginationToken, session: session)
     }
-}
 
-extension User {
     public static func blockingUsers(forUserID userID: User.ID, session: Session) async throws -> [Result<User, TwitterServerError>] {
         func blockingUsers(paginationToken: String?, previousPages: [Pagination<Result<User, TwitterServerError>>]) async throws -> [Pagination<Result<User, TwitterServerError>>] {
             let page = try await self.blockingUsers(forUserID: userID, pageCount: 1000, paginationToken: paginationToken, session: session)
