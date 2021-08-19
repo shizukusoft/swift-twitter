b0e670cfab652f84f86a7c46cb00041fd6fee2ca
Jaehong Kang <sinoru@me.com>
2021-08-19 12:03:18 +0900

    Support Twitter Archive's format

--- Sources/Twitter/Extensions/Foundation/JSONDecoder+Twitter.swift
+++ Sources/Twitter/Extensions/Foundation/JSONDecoder+Twitter.swift
@@ -7,14 +7,48 @@
 
 import Foundation
 
+extension JSONDecoder.DateDecodingStrategy {
+    static func formatted(_ dateFormatters: [DateFormatter]) -> Self {
+        .custom { decoder in
+            let container = try decoder.singleValueContainer()
+            let dateString = try container.decode(String.self)
+            
+            let date = dateFormatters
+                .lazy
+                .compactMap {
+                    $0.date(from: dateString)
+                }
+                .first
+            
+            if let date = date {
+                return date
+            } else {
+                throw DecodingError.dataCorruptedError(in: container, debugDescription: "Date string does not match format expected by formatter.")
+            }
+        }
+    }
+}
+
 extension JSONDecoder {
     public static var twt_default: JSONDecoder {
         let jsonDecoder = JSONDecoder()
-
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
-
-        jsonDecoder.dateDecodingStrategy = .formatted(dateFormatter)
+        
+        jsonDecoder.dateDecodingStrategy = .formatted([
+            {
+                let dateFormatter = DateFormatter()
+                dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+                dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
+                
+                return dateFormatter
+            }(),
+            {
+                let dateFormatter = DateFormatter()
+                dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+                dateFormatter.dateFormat = "EEE MMM d HH:mm:ss Z yyyy"
+                
+                return dateFormatter
+            }()
+        ])
 
         return jsonDecoder
     }
--- Sources/Twitter/Tweet.swift
+++ Sources/Twitter/Tweet.swift
@@ -23,4 +23,35 @@ extension Tweet: Decodable {
         case id, text
         case createdAt = "created_at"
     }
+    
+    enum CodingKeysV1: String, CodingKey {
+        case id = "id_str"
+        case fullText = "full_text"
+        case createdAt = "created_at"
+    }
+    
+    public init(from decoder: Decoder) throws {
+        do {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+
+            let id = try container.decode(String.self, forKey: .id)
+            let text = try container.decode(String.self, forKey: .text)
+            let createdAt = try container.decode(Date.self, forKey: .createdAt)
+            
+            self.id = id
+            self.text = text
+            self.createdAt = createdAt
+        } catch let originalError {
+            do {
+                let container = try decoder.container(keyedBy: CodingKeysV1.self)
+
+                self.id = try container.decode(String.self, forKey: .id)
+                self.text = try container.decode(String.self, forKey: .fullText)
+                self.createdAt = try container.decode(Date.self, forKey: .createdAt)
+            } catch {
+                debugPrint(error)
+                throw originalError
+            }
+        }
+    }
 }
