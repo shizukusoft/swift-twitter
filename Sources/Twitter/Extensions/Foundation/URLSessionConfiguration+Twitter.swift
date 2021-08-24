d9cf7b8a32bf04dc01212ad65a76e79b17e5ba3d
Jaehong Kang <sinoru@me.com>
2021-08-25 00:41:48 +0900

    Add Accept-Language, Accept-Encoding

--- Sources/Twitter/Extensions/Foundation/URLSessionConfiguration+Twitter.swift
+++ Sources/Twitter/Extensions/Foundation/URLSessionConfiguration+Twitter.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import OrderedCollections
 
 extension URLSessionConfiguration {
     public static var twt_default: URLSessionConfiguration {
@@ -14,7 +15,29 @@ extension URLSessionConfiguration {
 
         configuration.httpCookieStorage = ephemeral.httpCookieStorage
         configuration.urlCredentialStorage = ephemeral.urlCredentialStorage
+        
+        configuration.httpAdditionalHeaders = {
+            var httpAdditionalHeaders = configuration.httpAdditionalHeaders ?? [:]
+            
+            let preferredLanguages = OrderedSet(Locale.preferredLanguages.flatMap { [$0, $0.components(separatedBy: "-")[0]] } + ["*"])
+            httpAdditionalHeaders["Accept-Language"] = preferredLanguages.qualityJoined
+            
+            httpAdditionalHeaders["Accept-Encoding"] = OrderedSet(["br", "gzip", "deflate"]).qualityJoined
+            
+            return httpAdditionalHeaders
+        }()
 
         return configuration
+    }
+}
+
+extension Collection where Element == String {
+    var qualityJoined: String {
+        enumerated()
+            .map { offset, language in
+                let quality = 1.0 - ((Decimal(offset + 1)) / Decimal(count + 1))
+                return "\(language);q=\(quality)"
+            }
+            .joined(separator: ", ")
     }
 }
