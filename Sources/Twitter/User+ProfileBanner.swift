21f0daa2a4856d081eb76df2aabbb1d80bc226c2
Jaehong Kang <sinoru@me.com>
2021-10-03 16:31:46 +0900

    Add User+ProfileBanner

---
+++ Sources/Twitter/User+ProfileBanner.swift
@@ -0,0 +1,49 @@
+//
+//  User+ProfileBanner.swift
+//  
+//
+//  Created by Jaehong Kang on 2021/10/03.
+//
+
+import Foundation
+import OrderedCollections
+
+extension User {
+    public struct ProfileBanner {
+        public struct SizeClass {
+            let width: Double
+            let height: Double
+
+            let url: URL
+        }
+
+        public let sizes: OrderedDictionary<String, SizeClass>
+    }
+
+    public static func profileBanner(forUserID userID: User.ID, session: Session) async throws -> ProfileBanner {
+        var urlRequest = URLRequest(url: URL(twitterAPIURLWithPath: "1.1/users/profile_banner.json")!)
+        urlRequest.httpMethod = "GET"
+        urlRequest.urlComponents?.queryItems = [
+            URLQueryItem(name: "user_id", value: userID)
+        ]
+        await urlRequest.oauthSign(session: session)
+
+        let (data, _) = try await session.data(for: urlRequest)
+
+        return try JSONDecoder.twt_default.decode(ProfileBanner.self, from: data)
+    }
+
+    public func profileBanner(session: Session) async throws -> ProfileBanner {
+        try await Self.profileBanner(forUserID: id, session: session)
+    }
+}
+
+extension User.ProfileBanner: Decodable {}
+
+extension User.ProfileBanner.SizeClass: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case width = "w"
+        case height = "h"
+        case url = "url"
+    }
+}
